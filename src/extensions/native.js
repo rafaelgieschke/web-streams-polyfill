c891beb6e1f17cbd41e5e4b1f9dbede80330e9f9
Mattias Buelens <mattias.buelens@gmail.com>
2017-12-30 01:02:02 +0100

    Fix typo

--- src/extensions/native.js
+++ src/extensions/native.js
@@ -16,7 +16,7 @@ function IsStreamConstructor(ctor) {
 }
 
 export const NativeReadableStream = typeof ReadableStream === 'function' ? ReadableStream : undefined;
-export const NativeWritableStream = typeof WritableStream === 'function' ? ReadableStream : undefined;
+export const NativeWritableStream = typeof WritableStream === 'function' ? WritableStream : undefined;
 export const NativeTransformStream = typeof TransformStream === 'function' ? TransformStream : undefined;
 
 export function IsReadableStreamConstructor(ctor) {
