432c4290dc8a9bc266e989d32f5e515c38c96163
Mattias Buelens <mattias@buelens.com>
2018-02-01 00:59:26 +0100

    Switch back to default reader when handling pull without BYOB request

--- src/extensions/readable-wrapper.js
+++ src/extensions/readable-wrapper.js
@@ -1,13 +1,16 @@
 import { getBYOBOrDefaultReader } from './utils';
+import assert from '../polyfill/assert';
 
 export function createWrappingReadableSource(readable) {
+  // Check if input is a readable byte stream, i.e. it supports BYOB readers
   const { reader, mode } = getBYOBOrDefaultReader(readable);
+  reader.releaseLock();
 
   let source;
   if (mode === 'byob') {
-    source = new WrappingReadableByteStreamSource(reader);
+    source = new WrappingReadableByteStreamSource(readable);
   } else {
-    source = new WrappingReadableStreamDefaultSource(reader);
+    source = new WrappingReadableStreamDefaultSource(readable);
   }
 
   return source;
@@ -15,35 +18,66 @@ export function createWrappingReadableSource(readable) {
 
 class AbstractWrappingReadableStreamSource {
 
-  constructor(underlyingReader) {
-    this._underlyingReader = underlyingReader;
+  constructor(underlyingStream) {
+    this._underlyingStream = underlyingStream;
+    this._underlyingReader = undefined;
+    this._readerMode = undefined;
     this._readableStreamController = undefined;
   }
 
   start(controller) {
     this._readableStreamController = controller;
+  }
 
+  _attachDefaultReader() {
+    if (this._readerMode === 'default') {
+      return;
+    }
+
+    this._detachReader();
+
+    const reader = this._underlyingStream.getReader();
+    this._readerMode = 'default';
+    this._attachReader(reader);
+  }
+
+  _attachReader(reader) {
+    assert(this._underlyingReader === undefined);
+
+    this._underlyingReader = reader;
     this._underlyingReader.closed
       .then(() => {
-          this._readableStreamController.close();
+          if (reader === this._underlyingReader) {
+            this._readableStreamController.close();
+          }
         },
         reason => {
-          this._readableStreamController.error(reason);
+          if (reader === this._underlyingReader) {
+            this._readableStreamController.error(reason);
+          }
         })
       .catch(ignore => {
         // already closed or errored
       });
   }
 
+  _detachReader() {
+    if (this._underlyingReader === undefined) {
+      return;
+    }
+
+    this._underlyingReader.releaseLock();
+    this._underlyingReader = undefined;
+    this._readerMode = undefined;
+  }
+
   cancel(reason) {
     return this._underlyingReader.cancel(reason);
   }
 
-}
+  _pullWithDefaultReader() {
+    this._attachDefaultReader();
 
-class WrappingReadableStreamDefaultSource extends AbstractWrappingReadableStreamSource {
-
-  pull() {
     // TODO Backpressure?
     return this._underlyingReader.read()
       .then(({ value, done }) => {
@@ -58,6 +92,14 @@ class WrappingReadableStreamDefaultSource extends AbstractWrappingReadableStream
 
 }
 
+class WrappingReadableStreamDefaultSource extends AbstractWrappingReadableStreamSource {
+
+  pull() {
+    return this._pullWithDefaultReader();
+  }
+
+}
+
 function toUint8Array(view) {
   return new Uint8Array(view.buffer, view.byteOffset, view.byteLength);
 }
@@ -68,12 +110,26 @@ function copyArrayBufferView(from, to) {
   toArray.set(fromArray, 0);
 }
 
-const DEFAULT_CHUNK_SIZE = 1024;
-
 class WrappingReadableByteStreamSource extends AbstractWrappingReadableStreamSource {
+
+  constructor(underlyingStream) {
+    super(underlyingStream);
+  }
 
   get type() {
     return 'bytes';
+  }
+
+  _attachByobReader() {
+    if (this._readerMode === 'byob') {
+      return;
+    }
+
+    this._detachReader();
+
+    const reader = this._underlyingStream.getReader({ mode: 'byob' });
+    this._readerMode = 'byob';
+    this._attachReader(reader);
   }
 
   pull() {
@@ -81,10 +137,12 @@ class WrappingReadableByteStreamSource extends AbstractWrappingReadableStreamSou
     if (byobRequest !== undefined) {
       return this._pullWithByobRequest(byobRequest);
     }
-    return this._pullWithEnqueue();
+    return this._pullWithDefaultReader();
   }
 
   _pullWithByobRequest(byobRequest) {
+    this._attachByobReader();
+
     // reader.read(view) detaches the input view, therefore we cannot pass byobRequest.view directly
     // create a separate buffer to read into, then copy that to byobRequest.view
     const buffer = new Uint8Array(byobRequest.view.byteLength);
@@ -98,21 +156,6 @@ class WrappingReadableByteStreamSource extends AbstractWrappingReadableStreamSou
         } else {
           copyArrayBufferView(value, byobRequest.view);
           byobRequest.respond(value.byteLength);
-        }
-      });
-  }
-
-  _pullWithEnqueue() {
-    const buffer = new Uint8Array(DEFAULT_CHUNK_SIZE);
-
-    // TODO Backpressure?
-    return this._underlyingReader.read(buffer)
-      .then(({ value, done }) => {
-        const controller = this._readableStreamController;
-        if (done) {
-          controller.close();
-        } else {
-          controller.enqueue(value);
         }
       });
   }
